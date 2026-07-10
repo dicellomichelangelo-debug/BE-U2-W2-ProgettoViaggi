2c5bea7ff6b98a52b6572b4fad2109bebac151bd
dicellomichelangelo-debug <dicellomichelangelo@gmail.com>
2026-07-10 09:43:33 +0200

    Initial commit

---
+++ src/main/java/michelangelodicello/BE_U2_W2_ProgettoViaggi/BeU2W2ProgettoViaggiApplication.java
@@ -0,0 +1,13 @@
+package michelangelodicello.BE_U2_W2_ProgettoViaggi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeU2W2ProgettoViaggiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeU2W2ProgettoViaggiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/michelangelodicello/BE_U2_W2_ProgettoViaggi/BeU2W2ProgettoViaggiApplicationTests.java
@@ -0,0 +1,13 @@
+package michelangelodicello.BE_U2_W2_ProgettoViaggi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeU2W2ProgettoViaggiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
